9d7726c19a4c092df8846bbd041c72af7928b3bc
kagwanyu <kagwanyu1@gmail.com>
2023-08-01 22:49:55 +0300

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css  Changes not staged for commit: 	modified:   about-image.png 	modified:   home-image.png 	modified:   index.html 	modified:   style.css  Untracked files: 	.vscode/

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-window.addEventListener("scroll",function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0)
-})
+// window.addEventListener("scroll",function(){
+//     var header = document.querySelector("header");
+//     header.classList.toggle("sticky", window.scrollY > 0)
+// })
+
+// console.log('hi')
+document.addEventListener('scroll', () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+      console.log('Scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
